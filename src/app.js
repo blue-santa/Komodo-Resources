03ffe03a6fa9998f5b027bc9c78cc2d409b12e95
siddhartha <siddharthakomodo@gmail.com>
2018-06-06 13:01:52 -0800

    deploying to heroku

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const app = express();
 
+const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 
+appConfig.init();
 routeConfig.init(app);
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,24 @@
 const app = require("./app");
 const http = require("http");
-const server = http.createServer(app);
-const port = 3000;
 
-server.listen(port);
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+const server = http.createServer(app);
+
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+    if (isNaN(port)) {
+      return val;
+    }
+
+    if (port >= 0) {
+      return port;
+    }
+
+    return false;
+}
 
 server.on("listening", () => {
-  console.log(`server is listening for requests on port ${port}`);
+  console.log(`server is listening for requests on port ${server.address().port}`);
 });
