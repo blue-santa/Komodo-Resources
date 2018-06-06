342d63fc68ffd4d41885582b6a5aaf2f7e55fffa
siddhartha <siddharthakomodo@gmail.com>
2018-06-06 14:19:43 -0800

    heroku build

--- src/routes/static.js
+++ src/routes/static.js
@@ -6,7 +6,7 @@ router.get("/", (req, res, next) => {
 });
 
 router.get("/marco", (req, res, next) => {
-  res.send(`polo: ${process.env.megasecret}`);
+  res.send(`polo: ${process.env.secretTest}`);
 });
 
 module.exports = router;
