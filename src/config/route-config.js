7682a46170c90e06b721dee1cd7478fc33ea2720
siddhartha <siddharthakomodo@gmail.com>
2018-08-08 03:37:54 -0800

    thirdTopic file framework

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -3,9 +3,11 @@ module.exports = {
     const staticRoutes = require('../routes/static');
     const primaryTopicRoutes = require('../routes/primaryTopics');
     const secondaryTopicRoutes = require('../routes/secondaryTopics');
+    const thirdTopicRoutes = require('../routes/thirdTopics');
 
     app.use(staticRoutes);
     app.use(primaryTopicRoutes);
     app.use(secondaryTopicRoutes);
+    app.use(thirdTopicRoutes);
   }
 }
---
+++ src/routes/thirdTopics.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+const thirdTopicController = require('../controllers/thirdTopicController');
+
+module.exports = router;
